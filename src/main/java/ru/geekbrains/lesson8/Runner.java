8d3f785d83d000717d13599b8fc2b5fad078d979
geekbrains <info@geekbrains.ru>
2021-06-10 20:53:42 +0700

    Homeworck 8

---
+++ src/main/java/ru/geekbrains/lesson8/Runner.java
@@ -0,0 +1,57 @@
+package ru.geekbrains.lesson8;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        // task 1
+
+
+        // task 2
+
+
+        // task 3
+
+
+        // task 4
+
+
+        // task 5
+
+        // task 6
+
+
+        // task 7
+
+    }
+
+
+    /**
+     * task 1: description
+     */
+
+    /**
+     * task 2:
+     */
+
+    /**
+     * task 3:
+     */
+
+    /**
+     * task 4:
+     */
+
+    /**
+     * task 5:
+     */
+
+    /**
+     * task 6:
+     */
+
+    /**
+     * task 7:
+     */
+
+}
